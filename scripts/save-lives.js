18b877ff68976dc3eab46cfab9d2154ae798a370
George Cave <32631912+gcsalzburg@users.noreply.github.com>
2020-05-11 23:04:51 +0200

    Allow expanding text

--- scripts/save-lives.js
+++ scripts/save-lives.js
@@ -1,29 +1,61 @@
 const input_mappings = [
    {
       "input" : document.getElementById("meme_row1"),
-      "node"  : document.getElementById("svg_text1")
+      "node"  : document.getElementById("svg_text1"),
+      "max"   : 188,
+      "min"   : 120,
    },
    {
       "input" : document.getElementById("meme_row2"),
-      "node"  : document.getElementById("svg_text2")
+      "node"  : document.getElementById("svg_text2"),
+      "max"   : 234,
+      "min"   : 180,
    },
    {
       "input" : document.getElementById("meme_row3"),
-      "node"  : document.getElementById("svg_text3")
+      "node"  : document.getElementById("svg_text3"),
+      "max"   : 218,
+      "min"   : 160,
    },
    {
       "input" : document.getElementById("meme_row4"),
-      "node"  : document.getElementById("svg_text4")
+      "node"  : document.getElementById("svg_text4"),
+      "max"   : 187,
+      "min"   : 130,
    }
 ]
+const max_text_width = 1010;
+
 for(let i of input_mappings){
    i["input"].addEventListener("keyup",function(){
-      i["node"].textContent = i["input"].value.toUpperCase();
+      adjust_text(i);
    })
    i["input"].addEventListener("keydown",function(){
-      i["node"].textContent = i["input"].value.toUpperCase();
+      adjust_text(i);
    })
 }
+function adjust_text(i){
+   i["node"].textContent = i["input"].value.toUpperCase();
+   let width = i["node"].getBBox().width;
+   if(width > max_text_width){
+      let font_size = i["max"];
+      while( (font_size>i["min"]) && (width > max_text_width) ){
+         font_size--;
+         i["node"].style.fontSize = font_size+'px';
+         width = i["node"].getBBox().width;
+      }
+   }else{
+      let font_size = i["min"];
+      while( (font_size<i["max"]) && (width < max_text_width) ){
+         font_size++;
+         i["node"].style.fontSize = font_size+'px';
+         width = i["node"].getBBox().width;
+      }
+      font_size--;
+   }
+}
+
+
 const svg_obj = document.getElementById("poster_panel");
 const polygons = svg_obj.getElementsByTagName('polygon');
 document.getElementById("colour_marks").addEventListener("change", function(e){
